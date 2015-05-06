47ebfcb013bbc7d21702d3fd2b170d7140211d73
Mopsalarm <mopsalarm@users.noreply.github.com>
2015-05-06 19:48:06 +0200

    optimized cache

--- gif2webm.py
+++ gif2webm.py
@@ -1,30 +1,85 @@
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from base64 import urlsafe_b64decode
 
+import contextlib
 import hashlib
 import os
-import subprocess
-import contextlib
-import threading
-import traceback
-import uuid
-import shutil
 import re
+import shutil
+import subprocess
+import threading
+import uuid
+from base64 import urlsafe_b64decode
 
-from flask import abort, jsonify, send_file, Flask
 import pathlib
 import requests
 from concurrent import futures
+from flask import abort, jsonify, send_file, Flask
 
 
-class WebmCache(object):
+def wrap_in_future(func):
+    result = futures.Future()
+    try:
+        result.set_result(func())
+    except Exception as err:
+        result.set_exception(err)
+
+    return result
+
+
+class BlockingPool(object):
+    def __init__(self, max_workers):
+        self._pool = futures.ThreadPoolExecutor(max_workers)
+        self._lock = threading.RLock()
+        self._running = {}
+
+    def run(self, key, job):
+        with self._lock:
+            try:
+                return self._running[key]
+
+            except KeyError:
+                fast_result = wrap_in_future(job.fast)
+                if not fast_result.exception():
+                    return fast_result
+
+                # run job on pool
+                future = self._pool.submit(job.slow)
+                self._running[key] = future
+                future.add_done_callback(lambda f: self._remove(key))
+                return future
+
+    def _remove(self, key):
+        with self._lock:
+            self._running.pop(key)
+
+    @property
+    def running_count(self):
+        with self._lock:
+            return len(self._running)
+
+
+class FileJob(object):
+    def __init__(self, target, func):
+        self.target = target
+        self.func = func
+
+    def fast(self):
+        if not self.target.exists():
+            raise IOError("file {} not found".format(self.target))
+
+        return self.target
+
+    def slow(self):
+        self.func()
+        return self.target
+
+
+class WebmCache(BlockingPool):
     def __init__(self, output_path):
+        super(WebmCache, self).__init__(max_workers=2)
         self.output_path = output_path
-        self.pool = futures.ThreadPoolExecutor(max_workers=4)
-        self.lock = threading.RLock()
-        self.cache = {}
 
     def get(self, gif_url):
         """Takes an url to a gif file and returns
@@ -34,22 +89,16 @@ class WebmCache(object):
         :rtype: futures.Future
         """
         gwid = build_identifier(gif_url)
-        with self.lock:
-            if gwid in self.cache:
-                return self.cache[gwid]
-
-            future = self.pool.submit(self._do_convert, gwid, gif_url)
-            self.cache[gwid] = future
-            return future
+        webm = self.build_filename(gwid, "webm")
+        return self.run(gwid, FileJob(webm, lambda: self._do_convert(gwid, webm, gif_url)))
 
     def build_filename(self, gwid, extension):
         """:rtype: pathlib.Path"""
         return self.output_path / (gwid + "." + extension)
 
-    def _do_convert(self, gwid, gif_url):
+    def _do_convert(self, gwid, path_webm, gif_url):
         print("got gif {}: {}".format(gwid, gif_url))
         path_gif = self.build_filename(gwid, "gif")
-        path_webm = self.build_filename(gwid, "webm")
 
         # if it already exist, just return the file!
         if path_webm.exists():
@@ -84,43 +133,6 @@ def download_file(url):
     response = requests.get(url)
     response.raise_for_status()
     return response.content
-
-
-app = Flask(__name__)
-cache = WebmCache(pathlib.Path("webm"))
-
-
-def _convert(encoded_url):
-    gif_url = urlsafe_b64decode(encoded_url.encode("ascii"))
-    if not re.match(r"https?://[^/]*pr0gramm\.com/[^?#]+\.gif", gif_url.lower()):
-        print("Invalid url")
-        raise abort(403)
-
-    webm_path_future = cache.get(gif_url)
-    if webm_path_future.exception():
-        print("Got exception during conversion: {}".format(webm_path_future.exception()))
-        raise abort(500)
-
-    webm_path = webm_path_future.result()
-    print("webm found at {}".format(webm_path.absolute()))
-    return webm_path
-
-
-@app.route('/convert/<encoded_url>')
-def convert(encoded_url):
-    _convert(encoded_url)
-    return jsonify(path="/webm/{}/video.webm".format(encoded_url))
-
-
-@app.route("/webm/<encoded_url>/video.webm")
-def webm(encoded_url):
-    webm_path = _convert(encoded_url)
-    return send_file(str(webm_path), mimetype="video/webm")
-
-
-@app.route("/status")
-def status():
-    return jsonify(alive=True)
 
 
 def extract_gif_fps(gif):
@@ -158,11 +170,44 @@ def gif2webm(gif):
         shutil.rmtree(temp)
 
 
-# gifsicle -I 3769e63a9da3704d.gif
-# disposal asis delay 0.04s
-# convert -adjoin -coalesce 3769e63a9da3704d.gif frame.%04d.ppm
-# avconv -r 25 -i frame.%04d.ppm -c:v libvpx -f webm -b:v 400k -qmin 20 -qmax 42 -an -y output.webm
+def make_app():
+    app = Flask(__name__)
+    cache = WebmCache(pathlib.Path("webm"))
+
+    def _convert(encoded_url):
+        gif_url = urlsafe_b64decode(encoded_url.encode("ascii"))
+        if not re.match(r"https?://[^/]*pr0gramm\.com/[^?#]+\.gif", gif_url.lower()):
+            print("Invalid url")
+            raise abort(403)
+
+        webm_path_future = cache.get(gif_url)
+        if webm_path_future.exception():
+            print("Got exception during conversion: {}".format(webm_path_future.exception()))
+            raise abort(500)
+
+        webm_path = webm_path_future.result()
+        print("webm found at {}".format(webm_path.absolute()))
+        return webm_path
+
+
+    @app.route('/convert/<encoded_url>')
+    def convert(encoded_url):
+        _convert(encoded_url)
+        return jsonify(path="/webm/{}/video.webm".format(encoded_url))
+
+
+    @app.route("/webm/<encoded_url>/video.webm")
+    def webm(encoded_url):
+        webm_path = _convert(encoded_url)
+        return send_file(str(webm_path), mimetype="video/webm")
+
+
+    @app.route("/status")
+    def status():
+        return jsonify(alive=True, running=cache.running_count)
+
+    return app
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", threaded=True)
-    
+    make_app().run(host="0.0.0.0", threaded=True)
